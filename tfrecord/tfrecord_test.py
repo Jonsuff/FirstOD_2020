79343dd537c47d684949616745b8b03bbc3ebcc0
Jonsuff <dbwhdtjq92@gmail.com>
2020-03-11 12:09:11 +0900

    fixed data type

--- tfrecord/tfrecord_test.py
+++ tfrecord/tfrecord_test.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 import tfrecords
 import os
 import numpy as np
+from PIL import Image
+
 
 MAX_BOX_PER_IMAGE = 65
 PATH_IMAGES_HOME = "/home/rilab/workspace/cocotry/val2017"
@@ -13,12 +15,16 @@ file_list = os.listdir(PATH_IMAGES_HOME)
 
 def test_tfrecords_io(filename,frames):
     write_tfrecords(filename, frames)
-    dataset = read_tfrecords(filename, 2, 1, False)
+    dataset = read_tfrecords(filename, 1, 1, False)
     for i, feature in enumerate(dataset):
         image = feature["image"]
         bbox = feature["bbox"]
         print(f"{i} image: shape={image.get_shape()}, value={image.numpy()}")
         print(f"{i} bbox:  shape={bbox.get_shape()}, value={bbox[0, 0].numpy()}")
+        # save test image
+        rs = image.numpy().reshape(640,640,3)
+        result = Image.fromarray(rs)
+        result.save(f'test{i}.png')
 
 def write_tfrecords(filename, frames):
     print("="*10, "Write tfrecords")
@@ -27,13 +33,13 @@ def write_tfrecords(filename, frames):
     matched_box = TF_functions.matching_with_im_box(image_id,box_info)
     resized_box_annotations = TF_functions.resize_box_list(MAX_BOX_PER_IMAGE, matched_box)
     # resized_box_annotations_flat = [items for sublist in resized_box_annotations for items in sublist]
-    print(len(resized_box_annotations[1]))
     with tf.io.TFRecordWriter(filename) as writer:
         for i in range(frames):
             image_name_list = TF_functions.im_ids
             image_file_name = [file for file in file_list if file.endswith(("0000"+f"{image_name_list[i]}.jpg"))]
             open_image = TF_functions.open_image(PATH_IMAGES_HOME, image_file_name[0])
             resized_image = TF_functions.resize_image_list(open_image)
+            # print(resized_image)
             resized_image_data = resized_image.tostring()
             img_feature = tf.train.Feature(bytes_list=tf.train.BytesList(value=[resized_image_data]))
             raw_box = np.array(resized_box_annotations[i]).tostring()
@@ -65,7 +71,7 @@ def parse_example(example):
     # convert bytes to original type
     decoded["image"] = tf.io.decode_raw(parsed["image"], tf.uint8)
     decoded["bbox"] = tf.io.decode_raw(parsed["bbox"], tf.int64)
-    decoded["image"] = tf.reshape(decoded["image"], shape=(4,640, 640,3))
+    decoded["image"] = tf.reshape(decoded["image"], shape=(640, 640, 3))
     decoded["bbox"] = tf.reshape(decoded["bbox"], shape=(MAX_BOX_PER_IMAGE, 5))
     # you can do preprocess here, e.g. convert image type from uint8 (0~255) to float (0~1)
     # ...
@@ -81,4 +87,4 @@ def dataset_process(dataset, epoch, batch, shuffle):
 
 
 if __name__ == "__main__":
-    test_tfrecords_io("sample.tfrecords",10)
+    test_tfrecords_io("sample.tfrecords",5)
--- tfrecord/tfrecords.py
+++ tfrecord/tfrecords.py
@@ -3,9 +3,6 @@ import tensorflow as tf
 import json
 import os.path as op
 from PIL import Image
-import cv2
-
-
 
 
 class TfrecordMaker:
@@ -13,26 +10,9 @@ class TfrecordMaker:
         self.srcpath = srcpath
         self.filename_json = filename_json
         self.image_path = image_path
-        self.image = None
-        self.image_raw = None
-        self.image_reshaped_array = None
-        self.category_id = None
-        self.category_ids = []
-        self.box_x = None
-        self.box_y = None
-        self.box_w = None
-        self.box_h = None
-        self.box_total = []
-        self.box_with_same_id_list = []
-        self.box_resized = []
-        self.box_resized_list = []
         self.im_h = None
-        self.im_hs = []
         self.im_w = None
-        self.im_ws = []
-        self.im_id = None
         self.im_ids = []
-        self.im_id_annotations = None
         self.data_file = open(op.join(self.srcpath, self.filename_json))
         self.data = json.load(self.data_file)
         self.annotations_list = self.data['annotations']
@@ -50,60 +30,59 @@ class TfrecordMaker:
 
     #
     def open_image(self, image_path,filename_image):
-        self.image = Image.open(op.join(image_path, filename_image), 'r')
-        self.im_w, self.im_h = self.image.size
-        return self.image
+        image = Image.open(op.join(image_path, filename_image), 'r')
+        self.im_w, self.im_h = image.size
+        return image
 
     def get_image_annotations(self, frame):
         for i in range(frame):
-            self.im_id = self.image_list[i]['id']
-            self.im_ids.append(self.im_id)
-            self.im_w = self.image_list[i]['width']
-            self.im_ws.append(self.im_w)
-            self.im_h = self.image_list[i]['height']
-            self.im_hs.append(self.im_hs)
+            im_id = self.image_list[i]['id']
+            self.im_ids.append(im_id)
         return self.im_ids
 
     def get_bbox_annotations(self):
+        box_total = []
         for object_annotations in self.annotations_list:
-            self.im_id_annotations = object_annotations['image_id']
-            self.box_x = object_annotations['bbox'][0]
-            self.box_y = object_annotations['bbox'][1]
-            self.box_w = object_annotations['bbox'][2]
-            self.box_h = object_annotations['bbox'][3]
-            self.category_id = object_annotations['category_id']
-            box_list = [self.im_id_annotations, self.box_y, self.box_x, self.box_h, self.box_w, self.category_id]
-            self.box_total.append(box_list)
-        return self.box_total
+            im_id_annotations = object_annotations['image_id']
+            box_x = object_annotations['bbox'][0]
+            box_y = object_annotations['bbox'][1]
+            box_w = object_annotations['bbox'][2]
+            box_h = object_annotations['bbox'][3]
+            category_id = object_annotations['category_id']
+            box_list = [im_id_annotations, box_y, box_x, box_h, box_w, category_id]
+            box_total.append(box_list)
+        return box_total
 
     def matching_with_im_box(self, im_ids, box_total):
         same_id_box_list = []
+        box_with_same_id_list = []
         for image_id in im_ids:
             for box_info in box_total:
                 if image_id == box_info[0]:
                     del box_info[0]
                     same_id_box_list.append(box_info)
             box_with_same_id = same_id_box_list
-            self.box_with_same_id_list.append(box_with_same_id)
+            box_with_same_id_list.append(box_with_same_id)
             same_id_box_list = []
-        return self.box_with_same_id_list
+        return box_with_same_id_list
 
-    def resize_box_list(self, MAX_BOX, matched_box):
+    def resize_box_list(self, max_box, matched_box):
+        box_resized_list = []
         for box_list in matched_box:
             box_list_array = np.array(box_list,dtype=np.int32)
-            self.box_resized = np.zeros((MAX_BOX, 5), dtype=np.int32)
+            box_resized = np.zeros((max_box, 5), dtype=np.int32)
             box_count = len(box_list)
             if box_list_array.shape != (0,):
-                self.box_resized[:box_count] = box_list_array
-            self.box_resized_list.append(self.box_resized.tolist())
-        return self.box_resized_list
+                box_resized[:box_count] = box_list_array
+            box_resized_list.append(box_resized.tolist())
+        return box_resized_list
 
-    def resize_image_list(self,image_data):
-        image_pix_array = np.array(image_data.getdata(), dtype=np.int32)
+    def resize_image_list(self, image_data):
+        image_pix_array = np.array(image_data.getdata(), dtype=np.uint8)
         reshape_array = image_pix_array.reshape(self.im_h, self.im_w, 3)
-        resized_array = np.zeros((640,640,3), dtype=np.int32)
+        resized_array = np.zeros((640,640,3), dtype=np.uint8)
         if reshape_array.shape != (0,):
-            resized_array[:self.im_h,:self.im_w] = reshape_array
+            resized_array[:self.im_h, :self.im_w] = reshape_array
         return resized_array
 
 
