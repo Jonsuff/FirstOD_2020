1c620bddcbdb19fa2e84f9ce26517ae3dec7fef8
Jonsuff <dbwhdtjq92@gmail.com>
2020-05-06 16:08:20 +0900

    function updated

--- utils/data_process_utils.py
+++ utils/data_process_utils.py
@@ -2,26 +2,31 @@ import tensorflow as tf
 import numpy as np
 from setting.config import Config as cfg
 
-def box_processing(grtr):
+def box_scale_processing(grtr):
     # anchor_rate.shape = (9, 2)
-    anchor_rate = cfg.ANCHORS / cfg.SIZE_SQUARE
+    anchors = np.reshape(cfg.ANCHORS, [9,2])
+    anchor_rate = anchors / cfg.SIZE_SQUARE
     anchors_mask = [[6, 7, 8], [3, 4, 5], [0, 1, 2]]
 
     lbbox = np.zeros((cfg.SIZE_LBOX, cfg.SIZE_LBOX, 3, 4))
     mbbox = np.zeros((cfg.SIZE_MBOX, cfg.SIZE_MBOX, 3, 4))
     sbbox = np.zeros((cfg.SIZE_SBOX, cfg.SIZE_SBOX, 3, 4))
 
-    bbox_real = [lbbox, mbbox, sbbox]
+    bbox_real = [sbbox, mbbox, lbbox]
 
     grtr_wh = grtr[:,2:4]
     grtr_wh_new = grtr_wh[:, tf.newaxis, :]
     # grtr_wh.shape = (NUM_BOX, 1, 2)
     # calculate : (N, 1, 2) vs (9, 2)
     # result : (N, 9, 2)
+    # print(grtr_wh_new.shape)
     mins = np.maximum(-grtr_wh_new / 2, -anchor_rate / 2)
     maxs = np.minimum(grtr_wh_new / 2, anchor_rate / 2)
     inter_wh = maxs-mins
-    union_wh = (grtr_wh_new[:,:,0] * grtr_wh_new[:,:,1] + anchor_rate[:,:,0] * anchor_rate[:,:,1]
+    # print(grtr_wh_new[:,:,0])
+    # print(grtr_wh_new.shape)
+    # print(inter_wh.shape)
+    union_wh = (grtr_wh_new[:,:,0] * grtr_wh_new[:,:,1] + anchor_rate[:,0] * anchor_rate[:,1]
                 - inter_wh[:,:,0] * inter_wh[:,:,1])
     iou = (inter_wh[:,:,0] * inter_wh[:,:,1]) / union_wh
     # best_box_idx.shape = (NUM_BOX, )
@@ -36,7 +41,11 @@ def box_processing(grtr):
         grid_x = int(np.floor(grtr[i, 0] / scale[group]))
         grid_y = int(np.floor(grtr[i, 1] / scale[group]))
         index_k = anchors_mask[group].index(idx)
-        bbox_real[group][grid_y, grid_x ,index_k , :2] = grtr.numpy()[i, :2]
-        bbox_real[group][grid_y, grid_x ,index_k , 2:4] = grtr_wh.numpy()[i]
+        # print(grtr[i, :2]. shape)
+        # print(bbox_real[group][grid_y, grid_x ,index_k , :2].shape)
+        bbox_real[group][grid_y, grid_x ,index_k , :2] = grtr[i, :2]
+        bbox_real[group][grid_y, grid_x ,index_k , 2:4] = grtr_wh[i]
+
     return bbox_real
+
 
